1c8113cee0a7b4ea2afbd7ff99d8490db3dc1472
Abhishek-tech471 <abhishek.yadav_cs.bct23@gla.ac.in>
2025-02-22 21:11:41 +0530

    f and c edit

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./Contactus.css";
+import "./ContactUs.css";
 import { FaEnvelope, FaMapMarkerAlt, FaUserTie } from "react-icons/fa";
 
 const ContactUs = () => {
